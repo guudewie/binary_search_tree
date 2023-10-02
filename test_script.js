28605b207b723127ff6608be853ac3a7d067abd0
guudewie <philippthiessen12@gmail.com>
2023-10-02 16:17:44 +0200

    move prettyprint function to script file

--- test_script.js
+++ test_script.js
@@ -24,6 +24,19 @@ function createRandomArray() {
     return randomArray
 }
 
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+};
+
 
 let testTree = Tree(createRandomArray())
 testTree.prettyPrint(testTree.root)
